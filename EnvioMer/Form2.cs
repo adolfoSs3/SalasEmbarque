6de5af134f11633a605b4164ac0555d534642134
adolfoSs3 <angelo11219977@gmail.com>
2021-02-03 11:44:59 -0600

    we
    asss

--- EnvioMer/Form2.cs
+++ EnvioMer/Form2.cs
@@ -124,6 +124,7 @@ namespace EnvioMer
             catch (Exception EX)
             {
                 MessageBox.Show("Error" + EX.Message);
+                MessageBox.Show("che Culo we XD");
             }
 
 
